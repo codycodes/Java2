728d29a8045ba55560986bfaa4c50dc203bd5e27
Cody Gagnon <cgagno01@seattlecentral.edu>
2017-10-18 13:24:26 -0700

    10 - 18 (recursive palindrome) 2

--- class_code/10-18/RecursionExamples.java
+++ class_code/10-18/RecursionExamples.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
 
 public class RecursionExamples {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		while(true) {
+			System.out.println("Enter a phrase (lowercase, no punct.) or q to quit: ");
+			String s = scan.nextLine();
+			if (s.startsWith("q")) {
+				break;
+			} else {
+				// more readable? 
+//				if (isPalindrome(s)) { 
+//					System.out.println("\"" + s + "\" is a palindrome");
+//				} else {
+//					System.out.println("\"" + s + "\" isn't a palindrome");
+//				}
+				
+				Boolean b = isPalindrome(s);
+				System.out.println("\"" + s + "\" is" + (b ? " " : " not") + " a palindrome!");
+			}
+		}
+		scan.close(); // would close auto when main ends
 
 	}
 	
