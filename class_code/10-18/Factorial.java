b2618d4f93a73dfffe1cf212499cc20679d72ab0
Cody Gagnon <cgagno01@seattlecentral.edu>
2017-10-18 13:43:29 -0700

    10 - 18 (factorial)

--- class_code/10-18/Factorial.java
+++ class_code/10-18/Factorial.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
 
 public class Factorial {
 
 	
 	public static void main(String[] args) {
-		System.out.println(factorial(6));
+		Scanner scan = new Scanner(System.in);
+		while(true) {
+			
+			System.out.println("please enter an integer or press q to quit");
+			String s = scan.nextLine();
+			if (s.startsWith("q")) {
+				break;
+			} else {
+				int n = Integer.parseInt(s);
+				System.out.println(factorial(n));				
+			}
+		}
+		scan.close();
 	}
 	
+	// returns n! using a recursive formula
 	public static int factorial(int n) {
 		if (n == 1) {
 			return 1;
