c8685c64bff82b42e57bcc8165cfa9146da8a4e1
Cody Gagnon <cgagno01@seattlecentral.edu>
2017-10-18 13:31:28 -0700

    10 - 18 Factorial

---
+++ class_code/10-18/Factorial.java
@@ -0,0 +1,16 @@
+
+public class Factorial {
+
+	
+	public static void main(String[] args) {
+		System.out.println(factorial(6));
+	}
+	
+	public static int factorial(int n) {
+		if (n == 1) {
+			return 1;
+		} else {
+			return factorial(n - 1) * n;
+		}
+	}
+}
