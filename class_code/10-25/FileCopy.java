d357ccf15356c9116b2eeba8c7bdf53ff3a6d3a0
Cody Gagnon <cgagno01@seattlecentral.edu>
2017-10-25 13:38:21 -0700

    Multiple try-catch methods

--- class_code/10-25/FileCopy.java
+++ class_code/10-25/FileCopy.java
@@ -1,3 +1,8 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  *  Makes a copy of a file
  *
@@ -6,7 +11,23 @@
  */
 public class FileCopy {
 	public static void main(String[] args) {
+		String fileName = "src/FileCopy.java";
 		
+		// open an input stream
+		InputStream in;
+		try {
+			in = new FileInputStream(fileName);
+		} catch(FileNotFoundException e) {
+			System.out.println(fileName + " doesn't exist!");
+		}
+		
+		try (InputStream in2 = new FileInputStream(fileName)) {
+			
+		} catch(FileNotFoundException e) {
+			System.out.println(fileName + " could not be found");
+		} catch(IOException e) {
+			System.out.println("input stream could not be closed");
+		}
 	}
 
 }
