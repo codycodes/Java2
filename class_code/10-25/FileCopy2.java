f4d25bcef0d6316e5e8b66efe78a07f5b779cc4a
Cody Gagnon <cgagno01@seattlecentral.edu>
2017-10-25 14:27:00 -0700

    write(data) problem
    Full array is being written…

---
+++ class_code/10-25/FileCopy2.java
@@ -0,0 +1,23 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class FileCopy2 {
+	public static void main(String[] args) throws IOException {
+		InputStream in = new FileInputStream("src/FileCopy2.java");
+		OutputStream out = new FileOutputStream("copy2.txt");
+
+		byte[] data = new byte[500];
+
+		// number of bytes read
+		int n;
+		while ((n = in.read(data)) != -1) {
+			out.write(data);
+		}
+		in.close();
+		out.close();
+	}
+
+}
