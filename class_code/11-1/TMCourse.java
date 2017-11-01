63e9ebce7319e9df989037bfe4f246a78a656c98
Cody Gagnon <cgagno01@seattlecentral.edu>
2017-11-01 14:13:20 -0700

    Creating factory method TMCourse

--- class_code/11-1/TMCourse.java
+++ class_code/11-1/TMCourse.java
@@ -1,4 +1,6 @@
+import java.util.Scanner;
 import java.util.SortedMap;
+import java.util.TreeMap;
 
 /**
  * Representation of a full course for the Teaching Machine
@@ -18,6 +20,11 @@ public class TMCourse {
 	
 	// The user must use the factory method below to create
 	// a TMCourse object
-	
-	
+	public static TMCourse readCourse(Scanner scan) {
+		TMCourse course = new TMCourse();
+		course.title = scan.nextLine();
+		course.questions = new TreeMap<Integer, TMQuestion>();
+		
+		return course;
+	}
 }
