da99b7b25d288918f3bf5cd23a91409663a4fc51
Cody Gagnon <cgagno01@seattlecentral.edu>
2017-11-01 14:10:59 -0700

    Added TMCourse

---
+++ class_code/11-1/TMCourse.java
@@ -0,0 +1,23 @@
+import java.util.SortedMap;
+
+/**
+ * Representation of a full course for the Teaching Machine
+ * 
+ * @author codes
+ *
+ */
+public class TMCourse {
+	// title of the course
+	private String title;
+	// the questions in the course
+	SortedMap<Integer, TMQuestion> questions;
+	
+	// Hide the constructor
+	private TMCourse() {
+	}
+	
+	// The user must use the factory method below to create
+	// a TMCourse object
+	
+	
+}
