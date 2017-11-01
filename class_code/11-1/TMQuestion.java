562993f02584d80379476014ed22216c006d74ec
Cody Gagnon <cgagno01@seattlecentral.edu>
2017-11-01 13:23:09 -0700

    Added TMQuestion

---
+++ class_code/11-1/TMQuestion.java
@@ -0,0 +1,17 @@
+import java.util.Map;
+
+/**
+ * Representation of one question for the Teaching Machine
+ * 
+ * @author codes
+ * 
+ */
+public class TMQuestion {
+	// text of the question
+	private String text;
+	// question number
+	private int number;
+	// possible answers
+	private Map<String, Integer> answers;
+
+}
