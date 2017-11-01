108ee3a5cb68372d28ed21e40c5f9d83cd4807e4
Cody Gagnon <cgagno01@seattlecentral.edu>
2017-11-01 13:56:27 -0700

    Finished factory method

--- class_code/11-1/TMQuestion.java
+++ class_code/11-1/TMQuestion.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Representation of one question for the Teaching Machine
@@ -8,10 +11,39 @@ import java.util.Map;
  */
 public class TMQuestion {
 	// text of the question
-	private String text;
+	private String[] text; // made an array because that's how it's going to be in HW5
 	// question number
 	private int number;
 	// possible answers
 	private Map<String, Integer> answers;
+
+	// Hide the constructor from other classes
+	private TMQuestion() {
+
+	}
+
+	public static TMQuestion readQuestion(Scanner scan) {
+		TMQuestion q = new TMQuestion();
+		q.number = scan.nextInt();
+		// skip the new line after the int
+		scan.nextLine();
+		// text of the question
+		ArrayList<String> t = new ArrayList<String>();
+		String line;
+		while (!(line = scan.nextLine()).equals("-----")) {
+			t.add(line);
+		}
+		q.text = new String[t.size()];
+		t.toArray(q.text);
+		// possible answers
+		q.answers = new HashMap<String, Integer>();
+		while (scan.hasNextLine() && (line = scan.nextLine()).trim().isEmpty()) {
+			// format example: "true: 5"
+			String[] parts = line.split(":\\s*");
+			q.answers.put(parts[0].trim(), Integer.parseInt(parts[1].trim()));
+		}
+		
+		return q;
+	}
 
 }
