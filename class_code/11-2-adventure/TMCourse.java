886b96d135f07ca3d60f6865cf754e3bf2848525
Cody Gagnon <cgagno01@seattlecentral.edu>
2017-11-02 13:40:00 -0700

    Adventure Initial Upload

---
+++ class_code/11-2-adventure/TMCourse.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+/**
+ * Representation of a full course for the Teaching Machine
+ * 
+ * @author codes
+ *
+ */
+public class TMCourse {
+	// title of the course
+	private String title;
+	// the questions in the course
+	SortedMap<Integer, TMQuestion> questions;
+	
+	// Hide the constructor
+	private TMCourse() {
+	}
+	
+	// The user must use the factory method below to create
+	// a TMCourse object
+	public static TMCourse readCourse(Scanner scan) {
+		TMCourse course = new TMCourse();
+		course.title = scan.nextLine();
+		course.questions = new TreeMap<Integer, TMQuestion>();
+		while (scan.hasNextLine()) {
+			TMQuestion q = TMQuestion.readQuestion(scan);
+			System.out.println(q);
+			course.questions.put(q.getNumber(), q);
+		}
+		return course;
+	}
+	
+	/**
+	 * Returns the full description of the course as a string
+	 */
+	@Override
+	public String toString() {
+		String s = title + "\n";
+		for (Integer n : questions.keySet()) {
+			s += questions.get(n) + "\n";
+		}
+		return s;
+	}
+}
---
+++ class_code/11-2-adventure/TMMain.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TMMain {
+
+	public static void main(String[] args) throws IOException {
+		Scanner scan = new Scanner(new File("CrowtherRooms.txt"));
+		TMCourse course = TMCourse.readCourse(scan);
+		System.out.println(course);
+	}
+
+}
