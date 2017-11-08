8f1f47eca2d449110f805384f2602c1a28773c77
Cody Gagnon <cgagno01@seattlecentral.edu>
2017-11-08 14:46:42 -0800

    Added order of: O(n)

--- class_code/11-8_DIY-ArrayList/ComplexityExample.java
+++ class_code/11-8_DIY-ArrayList/ComplexityExample.java
@@ -1,14 +1,19 @@
 
 public class ComplexityExample {
 	// Write a method that takes an array of doubles and returns the maximum element
+
+	
+	
+	
+	
 	public double ComplexityExample(double[] a) {
-		double maxSoFar = a[0];
-		for (int i = 1; i < a.length; i++) {
-			if (maxSoFar < a[i]) {
-				maxSoFar = a[i];
+		double maxSoFar = a[0]; // (2) 
+		for (int i = 1; i < a.length; i++) { // int i = 1 (1), i < a.length (4), i++ (3)
+			if (maxSoFar < a[i]) {  // (4)
+				maxSoFar = a[i]; // (2)
 			}
 		}
-		return maxSoFar;
+		return maxSoFar; // (1)
 	}
-
+// 14n - 6
 }
