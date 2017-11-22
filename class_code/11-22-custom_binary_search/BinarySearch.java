9516863178d6ac881356119ea84ec6c9be3682b8
Cody Gagnon <cgagno01@seattlecentral.edu>
2017-11-22 13:28:17 -0800

    loopy style

--- class_code/11-22-custom_binary_search/BinarySearch.java
+++ class_code/11-22-custom_binary_search/BinarySearch.java
@@ -2,6 +2,20 @@
 public class BinarySearch {
 
 	public static int binarySearch(int[] a, int value) {
+		// With a loop
+		int first = 0, last = a.length - 1;
+		while(first <= last) {
+			int mid = (first + last) / 2;
+			//compare a[mid] with value
+			if (a[mid] < value) {
+				first = mid + 1;
+			} else if (a[mid] > value) {
+				last = mid - 1;
+			} else {
+				return mid;
+			}
+		}
+		return -(??) - 1; // -insertion point - 1
 	}
 
 }
